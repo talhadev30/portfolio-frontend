7f7d8e6bf671f9ad1417afa158be6a90c9996275
talha <m.talhasalman30@gmail.com>
2026-06-15 16:09:59 +0500

    update footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterTop from "./FooterTop"
 
 const Footer = () => {
   return (
-    <div className='lg:px-20 px-6 py-20 z-10 min-h-screen w-full bg-mist-300 text-black fixed bottom-0 left-0 '>
+    <div className='lg:px-20 px-6 py-40 z-10 min-h-screen w-full bg-mist-300 text-black fixed bottom-0 left-0 '>
       <FooterTop/>
       <FooterCenter/>
       <p className="m-10 text-center text-sm">© 2026 Talha Dev. All rights reserved. Powered by React. Hosted on Vercel.</p>
